aca289f6862fda9455514fdea3f348472b4349bc
Mateea Roman <115649249+mateea326@users.noreply.github.com>
2023-08-23 14:29:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,35 +3,87 @@ const c = canvas.getContext('2d');
 canvas.width = 1366;
 canvas.height = 725;
 
-c.fillRect(0, 0, canvas.width, canvas.height);
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 50) {
+    collisionsMap.push(collisions.slice(i, 50 + i))
+}
+
+class Boundary {
+    static width = 64;
+    static height = 64;
+    constructor({ position }) {
+        this.position = position;
+        this.width = 64;
+        this.height = 64;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const offset = {
+    x: -820,
+    y: -840
+}
+
+const boundaries = []
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 399)
+            boundaries.push(new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x,
+                    y: i * Boundary.height + offset.y
+                }
+            })
+            )
+    })
+})
 
 const image = new Image();
-image.src = './img/ThirdMap.png';
+image.src = './img/ForthMap.png';
 
 const playerImage = new Image();
 playerImage.src = './img/PlayerDown.png';
 
-const playerWidth = 700;
-const playerHeight = (playerWidth / playerImage.width) * playerImage.height;
-
-let width = playerImage.width;
-let height = playerImage.height;
-
-
 class Sprite {
-    constructor({ position, velocity, image }) {
+    constructor({ position, velocity, image, frames = { max: 1 } }) {
         this.position = position;
-        this.image = image
+        this.image = image;
+        this.frames = frames;
+        this.width= this.image.width / this.frames.max
     }
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y);
+        c.drawImage(this.image,
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height)
     }
 };
 
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - 700 / 4 / 2,
+        y: canvas.height / 2 - 168 / 2
+    },
+    image: playerImage,
+    frames: {
+        max: 4
+    }
+})
+
 const background = new Sprite({
     position: {
-        x: -820,
-        y: -840
+        x: offset.x,
+        y: offset.y
     },
     image: image
 })
@@ -51,51 +103,74 @@ const keys = {
     }
 }
 
-image.onload = function() {
-    animate();
-};
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
 
 function animate() {
     window.requestAnimationFrame(animate);
 
-    background.draw()
+    background.draw();
 
-    c.drawImage(playerImage,
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - playerImage.width / 2,
-        canvas.height / 2 - playerHeight / 2,
-        playerWidth / 4,
-        playerHeight);
+    //boundaries.forEach((boundary) => {
+    //  boundary.draw()
+    //})
 
-        if (keys.w.pressed) {
+    testBoundary.draw();
+
+    player.draw();
+
+    if (player.position.x + player.width >= testBoundary.position.x) {
+        console.log("colliding");
+    }
+
+    if (keys.w.pressed) {
+        {
             background.position.y += 3;
+            testBoundary.position.y += 3;
         }
-        if (keys.s.pressed) {
+    }
+    if (keys.s.pressed) {
+        {
             background.position.y -= 3;
+            testBoundary.position.y -= 3;
         }
-        if (keys.a.pressed) {
+    }
+    if (keys.a.pressed) {
+        {
             background.position.x += 3;
+            testBoundary.position.x += 3;
         }
-        if (keys.d.pressed) {
+    }
+    if (keys.d.pressed) {
+        {
             background.position.x -= 3;
+            testBoundary.position.x -= 3;
         }
+    }
 }
+
+animate();
 
 window.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'w':
+        case 'ArrowUp':
             keys.w.pressed = true
             break
         case 'a':
+        case 'ArrowLeft':
             keys.a.pressed = true
             break
         case 's':
+        case 'ArrowDown':
             keys.s.pressed = true
             break
         case 'd':
+        case 'ArrowRight':
             keys.d.pressed = true
             break
     }
@@ -104,18 +179,20 @@ window.addEventListener('keydown', (e) => {
 window.addEventListener('keyup', (e) => {
     switch (e.key) {
         case 'w':
+        case 'ArrowUp':
             keys.w.pressed = false
             break
         case 'a':
+        case 'ArrowLeft':
             keys.a.pressed = false
             break
         case 's':
+        case 'ArrowDown':
             keys.s.pressed = false
             break
         case 'd':
+        case 'ArrowRight':
             keys.d.pressed = false
             break
     }
 })
-
-
